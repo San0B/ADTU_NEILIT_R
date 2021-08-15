8d48e83d4b4f90ff21b9c8545534614a5e145e69
P.J.DAS@21 <paragjyotidas21@gmail.com>
2021-08-15 14:12:24 +0530

    Update Assignment_1_5.R

--- Lab1/Assignment_1_5.R
+++ Lab1/Assignment_1_5.R
@@ -1,48 +1,10 @@
-#6. Design a simple calculator in R.
+#5. WAP in R to find the sum of n natural numbers without formula.
 {
-ADD <- function() {
-  x <- as.integer(readline(prompt = "Enter first number: "))
-  y <- as.integer(readline(prompt = "Enter second number: "))
-  return(x + y)
+sumOfN <- function(n) {
+  return(n*(n+1)/2) 
 }
 
-SUBTRACT <- function() {
-  x <- as.integer(readline(prompt = "Enter first number: "))
-  y <- as.integer(readline(prompt = "Enter second number: "))
-  return(x - y)
-}
-
-
-MULTIPLY <- function() {
-  x <- as.integer(readline(prompt = "Enter first number: "))
-  y <- as.integer(readline(prompt = "Enter second number: "))
-  return(x * y)
-}
-
-
-DIVIDE <- function() {
-  x <- as.integer(readline(prompt = "Enter first number: "))
-  y <- as.integer(readline(prompt = "Enter second number: "))
-  return(x / y)
-}
-
-
-while(TRUE) {
-  print("##### SIMPLE CALCULATOR #####")
-  print("Enter 1 for addition")
-  print("Enter 2 for subtraction")
-  print("Enter 3 for multiplication")
-  print("Enter 4 for division")
-  print("Enter 5 to exit")
-  input <- readline()
-  result <- switch (input,
-                    "1" = ADD(),
-                    "2" = SUBTRACT(),
-                    "3" = MULTIPLY(),
-                    "4" = DIVIDE(),
-                    "5" = stop("Thank you!")
-  )
-  print(paste("result is : ", result))
-  print("===========================================")
-}
+n <- as.integer(readline(prompt = "Enter the n: "))
+sum <- sumOfN(n)
+print(paste("Sum is = ", sum))
 }
