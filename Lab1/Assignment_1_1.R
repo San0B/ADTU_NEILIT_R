3948c0885e4f457b5a4c46fafbc18de027dedc60
P.J.DAS@21 <paragjyotidas21@gmail.com>
2021-08-15 13:48:31 +0530

    Update Assignment_1_1.R

--- Lab1/Assignment_1_1.R
+++ Lab1/Assignment_1_1.R
@@ -4,7 +4,7 @@
     b <- as.integer(readline(prompt = "Enter the second number :"))
     c <- as.integer(readline(prompt = "Enter the third number :"))
 
-    if (a > b && a > b) {
+    if (a > b && a > c) {
       print(paste("The Greatest Number is :", a))
     } else if (b > c) {
       print(paste("The Greatest Number is :", b))
